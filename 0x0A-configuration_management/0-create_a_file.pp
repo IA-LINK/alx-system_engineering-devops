804c4e3207fbe141ad3bcc9ca01739d9dec49484
IA-LINK <jyc.kom@gmail.com>
2024-07-01 17:21:33 -0400

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#The code will create a file name school inside the /tmp Directory 
-file { "/tmp/school":
-    ensure  => file,
-    content => 'I love Puppet',
-    mode    => '07744',
-    owner   => 'www-data',
-    group   => 'www-data',
-} 
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
