6657cdfb02106f10ec88778613038507735d5cb3
ia-link <jyc.kom@gmail.com>
2024-08-14 14:39:10 -0700

    refix task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "ubuntu:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
-    else:
-        return 0
-    execpt requests.exceptions.RequestException:
-        return 0
